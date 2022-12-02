47cab36b5b10cc4c4f21c6147e037e251af3dec5
Ynyr Evans <sc20ye@leeds.ac.uk>
2022-12-02 12:29:54 +0000

    fixed flaw 2 now fuck you adam

--- src/main/java/comp3911/cwk2/AppServlet.java
+++ src/main/java/comp3911/cwk2/AppServlet.java
@@ -24,6 +24,10 @@ public class AppServlet extends HttpServlet {
   private static final String CONNECTION_URL = "jdbc:sqlite:db.sqlite3";
   private static final String AUTH_QUERY = "select * from user where username= ? and password= ?";
   private static final String SEARCH_QUERY = "select * from patient where surname= ? collate nocase";
+  private static final String JOIN_QUERY = "SELECT patient.id, patient.surname, patient.forename," +
+          " patient.address, patient.born, patient.gp_id, patient.treated_for FROM" +
+          " patient INNER JOIN user ON patient.gp_id=user.id AND user.username = ? " +
+          "AND patient.surname = ?";
 
   private final Configuration fm = new Configuration(Configuration.VERSION_2_3_28);
   private Connection database;
@@ -58,7 +62,7 @@ public class AppServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
-   throws ServletException, IOException {
+          throws ServletException, IOException {
     try {
       Template template = fm.getTemplate("login.html");
       template.process(null, response.getWriter());
@@ -72,8 +76,8 @@ public class AppServlet extends HttpServlet {
 
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
-   throws ServletException, IOException {
-     // Get form parameters
+          throws ServletException, IOException {
+    // Get form parameters
     String username = request.getParameter("username");
     String password = request.getParameter("password");
     String surname = request.getParameter("surname");
@@ -82,7 +86,7 @@ public class AppServlet extends HttpServlet {
       if (authenticated(username, password)) {
         // Get search results and merge with template
         Map<String, Object> model = new HashMap<>();
-        model.put("records", searchResults(surname));
+        model.put("records", searchResults(username, surname));
         Template template = fm.getTemplate("details.html");
         template.process(model, response.getWriter());
       }
@@ -111,15 +115,17 @@ public class AppServlet extends HttpServlet {
     }
   }
 
-  private List<Record> searchResults(String surname) throws SQLException {
+  private List<Record> searchResults(String username, String surname) throws SQLException {
     List<Record> records = new ArrayList<>();
-    PreparedStatement query = database.prepareStatement(SEARCH_QUERY);
-    query.setString(1,surname);
-    System.out.println(query);
+    PreparedStatement joinQuery = database.prepareStatement(JOIN_QUERY);
+    joinQuery.setString(1,username);
+    joinQuery.setString(2,surname);
+    System.out.println(joinQuery);
 
     try {
-      ResultSet results = query.executeQuery();
+      ResultSet results = joinQuery.executeQuery();
       while (results.next()) {
+        System.out.println(results);
         Record rec = new Record();
         rec.setSurname(results.getString(2));
         rec.setForename(results.getString(3));
@@ -131,7 +137,6 @@ public class AppServlet extends HttpServlet {
       }
 
     }catch (Exception ignored){
-
     }
     return records;
   }
